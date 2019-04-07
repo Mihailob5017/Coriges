aa8c9e8a558d8c1a1b814d86e463989470e000a5
Mihailo Boskovic <mihailo@govnar.gov>
2019-04-07 19:26:52 +0200

    finished the about page

---
+++ about/about.js
@@ -0,0 +1,23 @@
+var izbor=document.querySelectorAll("#choice > p");
+var ostali=document.querySelectorAll("#frame > div")
+function reset(){
+    for(var i=0;i<izbor.length;i++)
+    {
+        izbor[i].setAttribute("style","color: white");
+    }
+}
+
+for(var i=0;i<izbor.length;i++)
+{
+    izbor[i].addEventListener("click",function(){
+        reset();
+        this.style.color="darkred";
+    });
+}
+function prikazi(tekst){
+for(var i=0;i<ostali.length;i++)
+{
+    ostali[i].setAttribute("style","display: none");
+}    
+document.getElementById(tekst).style.display='inherit';
+}
